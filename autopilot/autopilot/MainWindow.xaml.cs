6c8bcf68a71baf66a2b1601bbc3032a19ca60663
scott246 <ndscott32@gmail.com>
2019-12-31 20:35:31 -0500

    treeview lists contents of directory

--- autopilot/autopilot/MainWindow.xaml.cs
+++ autopilot/autopilot/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,71 @@ namespace autopilot
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
+        private readonly string bindDirectory = @"E:\Code\autopilot\testbinds";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddDirectoryItemsToTreeView(TreeViewItem parentItem, string path)
+        {
+            foreach (string dir in Directory.EnumerateDirectories(path))
+            {
+                TreeViewItem item = new TreeViewItem
+                {
+                    Header = dir.Substring(dir.LastIndexOf('\\') + 1),
+                    Tag = dir,
+                    FontWeight = FontWeights.Bold
+                };
+
+                parentItem.Items.Add(item);
+
+                AddDirectoryItemsToTreeView(item, dir);
+            }
+
+            foreach (string file in Directory.EnumerateFiles(path))
+            {
+                TreeViewItem item = new TreeViewItem
+                {
+                    Header = file.Substring(file.LastIndexOf('\\') + 1),
+                    Tag = file,
+                    FontWeight = FontWeights.Normal
+                };
+
+                parentItem.Items.Add(item);
+            }
+        }
+
+        private void BindFolderTreeViewLoaded(object sender, RoutedEventArgs e)
+        {
+            Directory.CreateDirectory(bindDirectory);
+
+            foreach (string dir in Directory.EnumerateDirectories(bindDirectory))
+            {
+                TreeViewItem item = new TreeViewItem
+                {
+                    Header = dir.Substring(dir.LastIndexOf('\\') + 1),
+                    Tag = dir,
+                    FontWeight = FontWeights.Bold
+                };
+
+                bindFolderTreeView.Items.Add(item);
+                
+                AddDirectoryItemsToTreeView(item, dir);
+            }
+
+            foreach (string file in Directory.EnumerateFiles(bindDirectory))
+            {
+                TreeViewItem item = new TreeViewItem
+                {
+                    Header = file.Substring(file.LastIndexOf('\\') + 1),
+                    Tag = file,
+                    FontWeight = FontWeights.Normal
+                };
+
+                bindFolderTreeView.Items.Add(item);
+            }
+        }
 	}
 }
