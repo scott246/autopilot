a6a6a2e22acbb732db84d5b46a8e0cd3221c3620
Nathan Scott <ndscott32@gmail.com>
2021-05-11 00:32:52 -0400

    adding macroview back into editorview

--- autopilot/autopilot/Views/EditorView.xaml.cs
+++ autopilot/autopilot/Views/EditorView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using autopilot.Objects;
 using autopilot.Utils;
+using System;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
@@ -12,27 +13,103 @@ namespace autopilot.Views
 {
 	public partial class EditorView : Window
 	{
-		readonly ObservableCollection<Command> editorCommandListItems;
-		public EditorView(string title)
+		private ObservableCollection<Command> editorCommandListItems;
+		public EditorView()
 		{
 			InitializeComponent();
-			COMMAND_LIST = CommandUtils.GetAllActions();
 
-			MacroFile readingFile = MacroFileUtils.ReadMacroFile(title);
+			EditorPanel.Visibility = Visibility.Collapsed;
+
+			MacroViewUtils.LoadMacros();
+			MacroListView.ItemsSource = SORTED_FILTERED_MACRO_LIST;
+			SortComboBox.ItemsSource = SortFilterUtils.sortOptions;
+			SortComboBox.SelectedItem = SortFilterUtils.sortOptions[0];
+
+			COMMAND_LIST = CommandUtils.GetAllActions();
+		}
+
+		private void AboutMenuItem_Click(object sender, RoutedEventArgs e)
+		{
+			new AboutView().ShowDialog();
+		}
+
+		private void PreferencesMenuItem_Click(object sender, RoutedEventArgs e)
+		{
+			new PreferencesView().ShowDialog();
+		}
+
+		private void SortComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			MacroViewUtils.RefreshMacroList(MacroListView, SortComboBox.SelectedIndex, FilterTextBox.Text);
+		}
+
+		private void FilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			MacroViewUtils.RefreshMacroList(MacroListView, SortComboBox.SelectedIndex, FilterTextBox.Text);
+		}
+
+		private void AddMacroButton_Click(object sender, RoutedEventArgs e)
+		{
+			CustomDialogResponse response = CustomDialog.Display(CustomDialogType.OKCancel, "New Macro", "Name this macro.", textboxContent: "");
+			if (response.ButtonResponse != CustomDialogButtonResponse.Cancel && response.TextboxResponse.Trim() != "")
+			{
+				if (!MacroFileUtils.CreateMacro(MacroFileUtils.GetFileNameWithMacroExtension(response.TextboxResponse)))
+				{
+					CustomDialog.Display(CustomDialogType.OK, "Macro create error", "There is already a macro with this name.");
+				}
+			}
+
+			MacroViewUtils.RefreshMacroList(MacroListView, SortComboBox.SelectedIndex, FilterTextBox.Text);
+		}
+
+		private void DeleteMacroButton_Click(object sender, RoutedEventArgs e)
+		{
+			MacroFile selectedItem = (MacroFile)MacroListView.SelectedItem;
+			if (null != selectedItem && MacroViewUtils.ConfirmDeleteMacro(selectedItem))
+			{
+				try
+				{
+					string title = selectedItem.Title;
+					MacroFileUtils.DeleteMacroFile(title);
+				}
+				catch (UnauthorizedAccessException)
+				{
+					CustomDialog.Display(CustomDialogType.OK, "Insufficient Access", "Could not remove macro. Try running Autopilot as administrator.");
+				}
+				catch (Exception)
+				{
+					CustomDialog.Display(CustomDialogType.OK, "Macro Delete Error", "Could not remove macro.");
+				}
+			}
+			MacroViewUtils.RefreshMacroList(MacroListView, SortComboBox.SelectedIndex, FilterTextBox.Text);
+		}
+
+		private void MacroListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			string selectionTitle = ((MacroFile)MacroListView.SelectedItem).Title;
+
+			MacroFile readingFile = MacroFileUtils.ReadMacroFile(selectionTitle);
 
 			if (null == readingFile)
 			{
 				CustomDialog.Display(CustomDialogType.OK, "Read failure", "Failed to read macro file.");
-				Close();
 			}
 			else
 			{
-				string itemHeader = MacroFileUtils.GetFileNameWithNoMacroExtension(readingFile.Title);
-				EditorTitleTextBox.Text = itemHeader;
-				editorCommandListItems = new ObservableCollection<Command>(readingFile.Commands);
+				EditorTitleTextBox.Text = selectionTitle;
+				if (null == readingFile.Commands)
+				{
+					editorCommandListItems = new ObservableCollection<Command>();
+				}
+				else
+				{
+					editorCommandListItems = new ObservableCollection<Command>(readingFile.Commands);
+				}
 				EditorCommandList.ItemsSource = editorCommandListItems;
 				BindInputTextBox.Text = (readingFile.Bind != null && readingFile.Bind != "") ? readingFile.Bind : UNBOUND;
 			}
+
+			EditorPanel.Visibility = Visibility.Visible;
 		}
 
 		private void SaveMacroButton_Click(object sender, RoutedEventArgs e)
